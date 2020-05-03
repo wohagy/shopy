6fa5b6decb329934a6511087de58f4f875009733
wohagy <wohagy@gmail.com>
2020-05-03 06:07:32 +0300

    Stage 2

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,11 @@ var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
 var browsersync = require("browser-sync").create();
 var concat = require("gulp-concat");
+var style = [
+  "src/components/general_files/general.scss",
+  "src/components/header/style.scss",
+  "src/components/media/media.scss",
+];
 
 gulp.task("pug", function () {
   return gulp
@@ -20,15 +25,25 @@ gulp.task("pug", function () {
 });
 
 gulp.task("css_concat", function () {
-  return gulp
-    .src("src/components/header/style.scss")
-    .pipe(concat("style.scss"))
-    .pipe(gulp.dest("src/"));
+  return gulp.src(style).pipe(concat("style.scss")).pipe(gulp.dest("src/"));
 });
 
 gulp.task("sass", function () {
   return gulp
     .src("src/style.scss")
+    .pipe(sass({ outputStyle: "normal" }).on("error", sass.logError))
+    .pipe(autoprefixer(["last 10 versions"]))
+    .pipe(gulp.dest("build"))
+    .pipe(
+      browsersync.reload({
+        stream: true,
+      })
+    );
+});
+
+gulp.task("boots", function () {
+  return gulp
+    .src("src/components/bootstrap/scss/bootstrap.scss")
     .pipe(sass({ outputStyle: "normal" }).on("error", sass.logError))
     .pipe(autoprefixer(["last 10 versions"]))
     .pipe(gulp.dest("build"))
@@ -51,11 +66,16 @@ gulp.task("serve", function () {
 gulp.task("watch", function () {
   gulp.watch("src/index.pug", gulp.series("pug"));
   gulp.watch("src/components/header/index.pug", gulp.series("pug"));
-  gulp.watch("src/components/header/style.scss", gulp.series("css_concat"));
+  gulp.watch(style, gulp.series("css_concat"));
   gulp.watch("src/style.scss", gulp.series("sass"));
+  gulp.watch("src/components/bootstrap/scss/*.scss", gulp.series("boots"));
 });
 
 gulp.task(
   "default",
-  gulp.series(gulp.parallel("pug", "sass"), gulp.parallel("serve", "watch"))
+  gulp.series(
+    gulp.parallel("pug", "sass"),
+    gulp.parallel("serve", "watch"),
+    "boots"
+  )
 );
